49ccbe3b9b1fb70858c29be34da98c68b7448d3a
Elia Bonetto <eliabntt94@gmail.com>
2021-03-01 10:47:57 +0100

    ros req

---
+++ ros-req.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libamd2 libbtf1 libcamd2 libccolamd2 libcholmod3 libcxsparse3 libgraphblas1 libklu1 libldl2 libmetis5 librbio2 libspqr2 libsuitesparse-dev libumfpack5 ros-melodic-libg2o ros-melodic-find-object-2d
+sudo apt-get install ros-melodic-dwa-local-planner ros-melodic-cv-camera ros-melodic-eigenpy ros-melodic-eigen-stl-containers ros-melodic-grid-map ros-melodic-ddynamic-reconfigure ros-melodic-move-base  ros-melodic-costmap-converter ros-melodic-robot-localization  
+sudo apt-get install ros-melodic-freenect-camera ros-melodic-freenect-launch ros-melodic-global-planner ros-melodic-geometric-shapes ros-melodic-hector-gazebo ros-melodic-hector-imu* ros-melodic-marker-msgs
+sudo apt-get install ros-melodic-navigation ros-melodic-navigation-stage ros-melodic-object-recognition-msgs
+sudo apt-get install ros-melodic-octomap-ros* ros-melodic-octomap-msgs  ros-melodic-octomap-rviz-plugins* ros-melodic-octomap-server* ros-melodic-openni*
+sudo apt-get install ros-melodic-pose-cov-ops ros-melodic-random-numbers ros-melodic-robot-pose-ekf ros-melodic-rosapi ros-melodic-rosauth ros-melodic-rosbridge-library ros-melodic-rosbridge-msgs ros-melodic-rosbridge-server ros-melodic-roslint 
+sudo apt-get install ros-melodic-rqt ros-melodic-rqt-controller-manager ros-melodic-srdfdom ros-melodic-teleop-t* ros-melodic-warehouse-ros
